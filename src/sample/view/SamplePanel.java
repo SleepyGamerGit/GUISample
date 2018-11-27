e1cdafe19cf27bfdcb6a71619e40331a92477c05
Sleepy <hillsteadm@gmail.com>
2018-11-27 09:25:01 -0700

    button can now change color

--- src/sample/view/SamplePanel.java
+++ src/sample/view/SamplePanel.java
@@ -3,6 +3,9 @@ package sample.view;
 import sample.controller.GUIController;
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class SamplePanel extends JPanel
 {
@@ -39,6 +42,23 @@ public class SamplePanel extends JPanel
 
 	private void setupListeners()
 	{
+		colorButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent mouseClick)
+					{
+						changeBackgroundColor();
+					}
+				});
+	}
+
+	private void changeBackgroundColor() 
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
 		
+		this.setBackground(new Color(red, green, blue));
+		textLabel.setText("Red is: " + red + " Green is: " + "Blue is: " + blue);
 	}
 }
+
