30936670bb6932d923f8332b524226dc902be242
Sleepy <hillsteadm@gmail.com>
2018-11-27 08:48:26 -0700

    made more GUI

--- src/sample/controller/SampleGUIRunner.java
+++ src/sample/controller/SampleGUIRunner.java
@@ -5,6 +5,6 @@ public class SampleGUIRunner
 	public static void main (String[] args)
 	{
 		GUIController app = new GUIController();
-			app.start
+			app.start();
 	}
 }
---
+++ src/sample/view/SamplePanel.java
@@ -0,0 +1,44 @@
+package sample.view;
+
+import sample.controller.GUIController;
+import javax.swing.*;
+import java.awt.Color;
+
+public class SamplePanel extends JPanel
+{
+	private GUIController appController;
+	
+	private JButton colorButton;
+	private JLabel textLabel;
+	
+	public SamplePanel(GUIController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+		colorButton = new JButton("Click to change the color");
+		textLabel = new JLabel("This is a color app");		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		this.setBackground(Color.MAGENTA);
+		this.add(textLabel);
+		this.add(colorButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+
+	private void setupListeners()
+	{
+		
+	}
+}
